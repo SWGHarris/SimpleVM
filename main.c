85f299d5ebeda710a1593ba85c0884d15e765a7f
samWGHarris <65422108+samWGHarris@users.noreply.github.com>
2021-12-28 16:17:14 -0500

    registers, and some assembly instructions

---
+++ main.c
@@ -0,0 +1,148 @@
+int main(int argc, char **argv) {
+   uint16_t r0, r1, r2;
+   uint16_t imm5, imm_flag;
+   uint16_t pc_offset, offset;
+   int i, running;
+   enum { PC_START = 0x3000 };
+
+   reg[R_PC] = PC_START;
+
+   if (argc < 2) {
+      printf("lc3 [image-file] ...\n");
+      exit(EXIT_FAILURE);
+   }
+
+   for (i = 0; i < argc; i++) {
+      if (!read_image(argv[i])) {
+         printf("failed to load image: %\n", argv[i]);
+         exit(EXIT_FAILURE);
+      }
+   }
+
+   running = 1;
+   while (running) {
+      uint16_t instr = mem_read(reg[R_PC]++);
+      uint16_t op = instr >> 12;
+
+      switch (op) {
+         case OP_ADD:
+            r0 = (instr >> 9) & 0x7;
+            r1 = (instr >> 6) & 0x7;
+            imm_flag = (instr >> 5) & 0x1;
+
+            if (imm_flag) {
+               imm5 = sign_extend(instr & 0x1f, 5);
+               reg[r0] = reg[r1] + imm5;
+
+            } else {
+               r2 = instr & 0x7;
+               reg[r0] = reg[r1] + reg[r2];
+            }
+
+            update_flags(r0);
+            break;
+
+         case OP_AND:
+            r0 = (instr >> 9) & 0x7;
+            r1 = (instr >> 6) & 0x7;
+            imm_flag = (instr >> 5) & 0x1;
+
+            if (imm_flag) {
+               imm5 = sign_extend(instr & 0x1f, 5);
+               reg[r0] = reg[r1] & imm5;
+
+            } else {
+               r2 = instr & 0x7;
+               reg[r0] = reg[r1] & reg[r2];
+            }
+
+            update_flags(r0);
+            break;
+
+         case OP_NOT:
+            r0 = (instr >> 9) & 0x7;
+            r1 = (instr >> 6) & 0x7;
+            reg[r0] = ~reg[r1]
+            update_flags(r0);
+            break;
+
+         case OP_BR:
+            uint16_t neg = (instr >> 11) & 0x1;
+            uint16_t zro = (instr >> 10) & 0x1;
+            uint16_t pos = (instr >> 9) & 0x1;
+            uint16_t cond = reg[R_COND];
+            pc_offset = sign_extend(instr & 0x1ff, 9);
+
+            if ((neg && (cond == FL_NEG))
+                     || (zro && (cond == FL_ZRO))
+                     || (pos && (cond == FL_POS))) {
+               reg[R_PC] += pc_offset;
+
+            }
+            break;
+
+         case OP_JMP:
+            r0 = (instr >> 6) & 0x7;
+            if (r0 == 0x7) {
+               reg[R_PC] = reg[R_R7];
+
+            } else {
+               reg[R_PC] = reg[r0];
+
+            }
+            break;
+
+         case OP_JSR:
+            reg[R_R7] = reg[R_PC];
+
+            if ((instr >> 11) ^ 0x1) {
+               r0 = (instr >> 6) & 0x7;
+               reg[R_PC] = reg[r0];
+            } else {
+               pc_offset = sign_extend(instr & 0x7ff, 11);
+               reg[R_PC] = pc_offset;
+            }
+            break;
+
+         case OP_LD:
+            r0 = (instr >> 9) & 0x7;
+            pc_offset = sign_extend(instr & 0x1ff, 9);
+            reg[r0] = mem_read(reg[R_PC] + pc_offset);
+            update_flags(r0);
+            break;
+
+         case OP_LDI:
+            r0 = (instr >> 9) & 0x7;
+            pc_offset = sign_extend(instr & 0x1ff, 9);
+            reg[r0] = mem_read(mem_read(reg[R_PC] + pc_offset));
+            update_flags(r0);
+            break;
+
+         case OP_LDR:
+            r0 = (instr >> 9) & 0x7;
+            r1 = (instr >> 6) & 0x7;
+            offset = sign_extend(instr & 0x3f, 6);
+            break;
+         case OP_LEA:
+            {LEA, 7}
+            break;
+         case OP_ST:
+            {ST, 7}
+            break;
+         case OP_STI:
+            {STI, 7}
+            break;
+         case OP_STR:
+            {STR, 7}
+            break;
+         case OP_TRAP:
+            {TRAP, 8}
+            break;
+         case OP_RES:
+         case OP_RTI:
+         default:
+            {BAD OPCODE, 7}
+            break;
+      }
+   }
+}
